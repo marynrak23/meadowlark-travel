43288fc416473ef84c2c8b7584e33083c44f6a6b
marynrak <marynrak@gmail.com>
2022-07-06 15:22:51 +0300

    Adding POST method (newsletter page) that gives arguments in console

--- meadowlark.js
+++ meadowlark.js
@@ -4,7 +4,12 @@ const chalk = require('chalk')
 const fortune = require('./lib/fortune')
 const handlers = require('./lib/handlers')
 
+const bodyParser = require('body-parser')
+
 const app = express()
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const port = process.env.PORT || 3000
 
@@ -19,9 +24,29 @@ app.get('/', handlers.home)
 
 app.get('/about', handlers.about)
 
+app.get('/headers', (req, res) => {
+    res.type('text/plain')
+    const headers = Object.entries(req.headers)
+        .map(([key, value]) => `${key}: ${value}`)
+    console.log('---------------------------')
+    console.log(chalk.bgYellow('BROWSER HEADERS INFO'))
+    console.log(headers.join('\n'))
+    res.send('Headers info console')
+})
+
+app.get('/newsletter-signup', handlers.newsletterSignup)
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
+
 app.use(handlers.notFound)
 
 app.use(handlers.serverError)
+
+
+// TODO: For server security to disable response headers
+/*
+app.disable('x-powered-by')
+*/
 
 if(require.main === module) {
     app.listen(port, () => console.log(
